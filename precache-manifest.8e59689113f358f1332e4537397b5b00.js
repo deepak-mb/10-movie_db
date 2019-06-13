14a93d4f06e0104bb33fd40e49ffc76b02692b92
Deepak <deepak25mb@gmail.com>
2019-06-13 18:57:14 +0530

    Updates

---
+++ precache-manifest.8e59689113f358f1332e4537397b5b00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d9f7a58ecb1cf66be5ad1ff9344ca1",
+    "url": "/10-movie_db/index.html"
+  },
+  {
+    "revision": "a240eadc27371ffee796",
+    "url": "/10-movie_db/static/css/main.ef901846.chunk.css"
+  },
+  {
+    "revision": "59a323e718a79b15bbe6",
+    "url": "/10-movie_db/static/js/2.9e627b49.chunk.js"
+  },
+  {
+    "revision": "a240eadc27371ffee796",
+    "url": "/10-movie_db/static/js/main.84f69f1c.chunk.js"
+  },
+  {
+    "revision": "6d0235e20fef92a9d418",
+    "url": "/10-movie_db/static/js/runtime~main.50a1fd1a.js"
+  },
+  {
+    "revision": "8ba3391eb9f58ef83b959400b15a409b",
+    "url": "/10-movie_db/static/media/imdb.8ba3391e.svg"
+  }
+]);
